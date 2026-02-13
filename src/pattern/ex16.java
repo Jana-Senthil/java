8583e7029e9392ec471c29fc65204974ef36a74d
Jana-Senthil <janasenthil1008@gmail.com>
2026-02-13 21:31:51 +0530

    ex16 and ex17

--- src/pattern/ex16.java
+++ src/pattern/ex16.java
@@ -18,9 +18,13 @@ public class ex16 {
         int n= input.nextInt();
         for (int i = 1; i <= 2*n; i++) {
             for (int j = 1; j <= n; j++) {
-
+                if((j > n-i && i <= n) || (i > n && j > i-n)) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
             }
-
+            System.out.println();
         }
     }
 }
--- src/pattern/ex17.java
+++ src/pattern/ex17.java
@@ -1,4 +1,29 @@
 package pattern;
 
+import java.util.Scanner;
+
 public class ex17 {
+    public static void main(String[] args) {
+
+//             n=4
+//             *
+//            ***
+//           *****
+//          *******
+//           *****
+//            ***
+//             *
+        Scanner input = new Scanner(System.in);
+        int n= input.nextInt();
+        for (int i = 1; i <= n*2-1; i++) {
+            for (int j = 1; j <= n*2-1; j++) {
+                if((i <=n && j >= n-i+1 && j < n+i) || ( i > n && j > i-n && j < 2*n-(i-n) )){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
